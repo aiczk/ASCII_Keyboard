d4a67fae406f9b3f9274de9cb628e020fb5fbf6c
BelieberNeo <aiczk.398@gmail.com>
2020-08-18 04:00:20 +0900

    bug fixed.

--- bps.ino
+++ bps.ino
@@ -4,9 +4,9 @@ const int rowNum = 2;
 const int colNum = 4;
 const int rowPin[rowNum] = { 3, 4 };
 const int colPin[colNum] = { 5, 6, 7, 8 };
+const byte Null = B00000000;
 
 byte chord, cache;
-bool isPress;
 int index;
 
 void setup()
@@ -20,8 +20,6 @@ void setup()
   for(int i = 0; i < colNum; i++)
     pinMode(colPin[i], INPUT_PULLUP);
 
-  index = -1;
-
   Serial.begin(9600);
 }
 
@@ -32,45 +30,41 @@ void loop()
   {
     digitalWrite(rowPin[i], LOW);
 
-    for(int j = colNum; j < 0; j--)
+    for(int j = colNum; j < 0; j--, ++index)
     {
-      ++index;
       if(digitalRead(colPin[j]) != LOW)
       { 
         //キーの状態が以前と異なっていたら対象bitを0にする
         if(((chord >> index) & 1) != 1)
           continue;
-
-        //何らかのキーが押されているので1bit右シフト
-        if(isPress)
-          cache = cache << 1;
         
         chord &= ~(1 << index);
         continue;
       }
 
       //対象bitを1にする
-      chord = cache |= (1 << index);
-      isPress = true;
+      chord |= (1 << index);
     }
 
     digitalWrite(rowPin[i], HIGH);
   }
-
-  Serial.print("CURRENT: ")
-  Serial.print(chord);
-  index = -1;
-
-  //フラグが立っており、NULL(全てのbitが0である)なら処理継続
-  //NULLでない(全てのbitが0でない)ならreturn
-  if(isPress && cache & B00000000)
-    isPress = false;
-  else
+  
+  if(cache == Null)
+  {
+    cache = chord;
     return;
+  }
+
+  //2回同じ値だったら次の処理へ
+  if(cache != chord)
+    return;
+
+  //debug
+  Serial.print(chord);
 
   //chordをシリアルポートに書き込む
   Serial1.write(chord);
 
   //すべてのbitを0に戻す
-  chord = cache = B00000000;
+  chord = cache = Null;
 }
